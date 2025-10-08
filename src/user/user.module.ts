88b826cfadbf3b7cf926b21f8462a668a2d7bbbc
hurstin <132925282+hurstin@users.noreply.github.com>
2025-10-08 19:13:35 +0100

    changing absolute path to relative

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,8 +3,8 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
-import { NotificationModule } from 'src/notification/notification.module';
+import { CloudinaryService } from '../cloudinary/cloudinary.service';
+import { NotificationModule } from '../notification/notification.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
